e5d4d2967e5a675ec4021c8b67134ac0233f1725
Gabriel Haddad <gabriel.haddad@dtidigital.com.br>
2019-11-13 09:38:31 -0300

    inclui alterações na master

---
+++ js/main-home.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  $("#qtd-kg-carne").on("change", function (e) {
+    $("#descarga").html('');
+    $("#banho").html('');
+    $("#maquina-lavar").html('');
+    $("#lavar-carro").html('');
+  });
+});
